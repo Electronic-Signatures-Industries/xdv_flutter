7c7e66bbb44139da4af3b7a3f720131bae964777
Rogelio <molekilla@gmail.com>
2020-11-21 16:18:26 -0500

    ipfs

--- wallet/lib/src/ipfs/ipfs.dart
+++ wallet/lib/src/ipfs/ipfs.dart
@@ -1,6 +1,75 @@
 import 'package:wallet/src/wallet_base.dart';
+import 'package:restio/restio.dart';
 
-import 'package:js/js.dart';
+// https://github.com/tiagohm/restio/blob/master/client/README.md
+class IPFS {
+  final client = Restio();
+  var URL = 'https://ipfs-api.xdv.digital/api/v0';
+  constructor() {}
 
-// https://codeburst.io/how-to-use-javascript-libraries-in-your-dart-applications-e44668b8595d
-class IpfsWallet extends Wallet {}
+// https://github.com/ipfs/js-ipfs/blob/master/docs/core-api/FILES.md#ipfsadddata-options
+  addFile() async {
+    final request = Request(
+      uri: RequestUri.parse('$URL/add'),
+      method: HttpMethod.post,
+    );
+    final call = client.newCall(request);
+    final response = await call.execute();
+    await response.close();
+  }
+
+// https://github.com/ipfs/js-ipfs/blob/master/docs/core-api/PIN.md#ipfspinaddipfspath-options
+  pin() async {
+    final request = Request(
+      uri: RequestUri.parse('$URL/pin/add'),
+      method: HttpMethod.post,
+    );
+    final call = client.newCall(request);
+    final response = await call.execute();
+    await response.close();
+  }
+
+// https://github.com/ipfs/js-ipfs/blob/master/docs/core-api/NAME.md#ipfsnamepublishvalue-options
+  publish() async {
+    final request = Request(
+      uri: RequestUri.parse('$URL/name/publish'),
+      method: HttpMethod.post,
+    );
+    final call = client.newCall(request);
+    final response = await call.execute();
+    await response.close();
+  }
+
+  // https://github.com/ipfs/js-ipfs/blob/master/docs/core-api/FILES.md#ipfsadddata-options
+  addTar() async {
+    final request = Request(
+      uri: RequestUri.parse('$URL/tar/add'),
+      method: HttpMethod.post,
+    );
+    final call = client.newCall(request);
+    final response = await call.execute();
+    await response.close();
+  }
+
+  // https://github.com/ipfs/js-ipfs/blob/master/docs/core-api/FILES.md#ipfsadddata-options
+  fetch() async {
+    final request = Request(
+      uri: RequestUri.parse('https://ipfs-api.xdv.digital/api/v0'),
+      method: HttpMethod.post,
+    );
+    final call = client.newCall(request);
+    final response = await call.execute();
+    await response.close();
+  }
+
+  // DAG
+  // https://github.com/ipfs/js-ipfs/blob/master/docs/core-api/DAG.md
+
+  // resolve
+
+  // tree
+
+  // get
+
+  // put
+}
